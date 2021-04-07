69770194bac3004de4fd7f248260d4f7b7340e41
Dotdane <dotdane2@yahoo.com>
2021-04-07 14:49:26 -0500

    Deleted Timer component usage

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-main>
+      <git-hub />
+      <grid />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Grid from "./components/Grid";
+import GitHub from "./components/GitHub";
+
+export default {
+  name: "App",
+
+  components: {
+    Grid,
+    GitHub
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
+<style>
+#app {
+  background: #232222;
+}
+</style>
+
+/**
+app background
+card theme
+on hover card
+ */
