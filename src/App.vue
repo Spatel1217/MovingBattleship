e849f598ade6c6186893e60ee8cd9a13b5f6f7af
Shalu Patel <shalu.patel1217@gmail.com>
2021-04-16 14:25:20 -0500

    imported the working commandbox from the old project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CommandBox />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CommandBox from "./components/CommandBox";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
+    CommandBox
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/CommandBox.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <label>Commands:</label>
+    <div v-for="order in previousCommands" :key="order" class="chat">
+      {{ order }}
+    </div>
+    <input type="text" v-model="currentCommand" @keyup.enter="registerCommand" placeholder="Enter a Command">
+  </div>
+  <div v-if="inputError" class="error">
+    {{ inputError }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentCommand: '',
+      previousCommands: [],
+      inputError: ''
+    }
+  },
+  methods: {
+    registerCommand() { //logic for checking commands for validity should also go here
+      this.previousCommands.push(this.currentCommand)
+      this.currentCommand = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  display: block;
+  padding: 10px 6px;
+  box-sizing: border-box;
+  border: 3px solid #151513;
+  width: 25%;
+}
+.chat {
+  padding: 1px 1px;
+  border: 1px solid black;
+  margin-bottom: 5px;
+  width: 25%;
+}
+
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
+</style>
