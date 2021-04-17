4b156fbde2292b93fef1fdda690d56107349cc6f
Dino Weinstock <Dotdane2@yahoo.com>
2021-04-17 17:06:24 -0500

    Added Grid.vue with simple clickable cells not connected to commandbox yet. Adapted from Ahoy Map.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
+  <Grid />
   <CommandBox />
 </template>
 
 <script>
 import CommandBox from "./components/CommandBox";
+import Grid from "./components/Grid";
 
 export default {
   name: "App",
 
   components: {
-    CommandBox
+    CommandBox,
+    Grid
+  },
+  data: function() {
+    return {
+      display: this.onResize()
+    };
+  },
+  methods: {
+    onResize() {
+      if (window.innerWidth > 1100) {
+        this.display = false;
+      } else {
+        this.display = true;
+      }
+    }
+  },
+  created() {
+    window.addEventListener("resize", this.onResize);
   }
 }
 </script>
