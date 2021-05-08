1e6ceb107e95e5d7df6d15de642aea2bf7c656cb
Shalu Patel <shalu.patel1217@gmail.com>
2021-05-07 19:22:50 -0500

    fixed Merge

--- src/Classes/BoatGroup.js
+++ src/Classes/BoatGroup.js
@@ -26,7 +26,6 @@ class BoatGroup {
 
     //Setup 5 boats with fixed sizes and fixed starting positions and orientations for initial testing
     addFixedBoats() {
-<<<<<<< HEAD
         this.boats.push(new Boat(1, 1, 1, false, 0));
         this.boats.push(new Boat(3, 2, 2, true, 1));
         this.boats.push(new Boat(5, 5, 3, false, 2));
@@ -90,16 +89,6 @@ class BoatGroup {
                 }
             }
         }
-=======
-        this.boats.push(new Boat(1, 1, 1, 0));
-        this.boats[0].flip(); // vertical
-        this.boats.push(new Boat(3, 2, 2, 1));
-        this.boats.push(new Boat(5, 5, 3, 2));
-        this.boats[2].flip(); //vertical
-        this.boats.push(new Boat(8, 2, 4, 3));
-        this.boats[3].flip(); //vertical
-        this.boats.push(new Boat(2, 9, 5, 4));
->>>>>>> b1a6eb22fb4081a051141db23719337221e7d290
     }
 
     addBoat(boat) {
